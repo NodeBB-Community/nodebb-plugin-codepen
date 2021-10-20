a148762510301d31ebd06525855b0b949db3d371
Julian Lam <julian@nodebb.org>
2021-10-20 16:06:31 -0400

    style: eslint

--- library.js
+++ library.js
@@ -1,26 +1,26 @@
-(function(module) {
-    "use strict";
+'use strict';
 
-    const codepen = {};
-    const embed = '<p class="codepen" data-height="300" data-default-tab="html,result" data-slug-hash="$2" data-preview="true" data-user="$1" style="height: 300px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;"></p>';
-    var pen = /<a href="(?:https?:\/\/)?codepen\.io\/(.+)\/pen\/([\w\-_]+)".+?<\/a>/g;
+(function (module) {
+	const codepen = {};
+	const embed = '<p class="codepen" data-height="300" data-default-tab="html,result" data-slug-hash="$2" data-preview="true" data-user="$1" style="height: 300px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;"></p>';
+	const pen = /<a href="(?:https?:\/\/)?codepen\.io\/(.+)\/pen\/([\w\-_]+)".+?<\/a>/g;
 
-    codepen.parse = function(data, callback) {
-        if (!data || !data.postData || !data.postData.content) {
-            return callback(null, data);
-        }
-        if (data.postData.content.match(pen)) {
-            data.postData.content = data.postData.content.replace(pen, embed);
-        }
+	codepen.parse = function (data, callback) {
+		if (!data || !data.postData || !data.postData.content) {
+			return callback(null, data);
+		}
+		if (data.postData.content.match(pen)) {
+			data.postData.content = data.postData.content.replace(pen, embed);
+		}
 
-        callback(null, data);
-    };
+		callback(null, data);
+	};
 
-    codepen.addEmbedScript = (scripts) => {
-        scripts.push('https://cpwebassets.codepen.io/assets/embed/ei.js');
+	codepen.addEmbedScript = (scripts) => {
+		scripts.push('https://cpwebassets.codepen.io/assets/embed/ei.js');
 
-        return scripts;
-    }
+		return scripts;
+	};
 
-    module.exports = codepen;
+	module.exports = codepen;
 }(module));
